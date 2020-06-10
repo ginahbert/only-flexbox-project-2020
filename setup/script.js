683bfe1bd9580e9df006fb35e358651c1966a1fd
ginahbert <59503219+ginahbert@users.noreply.github.com>
2020-06-10 13:43:13 +0200

    initial commit

---
+++ setup/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    $('#menu-btn').click(changeNav);
+
+    function changeNav(){
+        $('#links').toggle(2000);
+        $('#menu-btn').toggleClass('turn')
+    }
+
+})
